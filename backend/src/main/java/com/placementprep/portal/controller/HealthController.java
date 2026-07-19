456b5dc5b0730f04708cca804d0bbeffac6a3a48
KK <Kksingh2228@gmail.com>
2026-07-19 02:43:45 +0000

    Make backend Render-ready: dynamic PORT, healthCheck, broaden CORS

--- backend/src/main/java/com/placementprep/portal/controller/HealthController.java
+++ backend/src/main/java/com/placementprep/portal/controller/HealthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthController {
 
     @GetMapping("/")
